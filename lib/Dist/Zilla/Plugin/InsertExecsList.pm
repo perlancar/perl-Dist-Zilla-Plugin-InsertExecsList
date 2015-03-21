ec474b18d3d09e6c3bdc721da2abefadd01b6f56
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2015-03-21 12:32:29 +0700

    Fixes

--- lib/Dist/Zilla/Plugin/InsertExecsList.pm
+++ lib/Dist/Zilla/Plugin/InsertExecsList.pm
@@ -27,7 +27,7 @@ sub munge_file {
     my ($self, $file) = @_;
     my $content = $file->content;
     if ($content =~ s{^#\s*INSERT_EXECS_LIST\s*$}{$self->_insert_execs_list($1, $2)."\n"}egm) {
-        $self->log(["inserting execs list into '%s'", $1, $2, $file->name]);
+        $self->log(["inserting execs list into '%s'", $file->name]);
         $file->content($content);
     }
 }
@@ -42,15 +42,15 @@ sub _insert_execs_list {
     for my $file (@{ $self->found_files }) {
         my $name = $file->name;
         next if $name =~ s!^lib[/\\]!!;
-        $name =~ s![/\\]!::!g;
-        push @list,
+        $name =~ s!.+[/\\]!!;
+        push @list, $name;
     }
     @list = sort @list;
 
     join(
         "",
         "=over\n\n",
-        (map {"=item * L<>\n\n"} @list),
+        (map {"=item * L<$_>\n\n"} @list),
         "=back\n\n",
     );
 }
